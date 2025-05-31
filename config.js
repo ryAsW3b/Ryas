52020559f495925f5e3bf5e9bfc1ac11262566c3
ryAsW3b <82412427+ryAsW3b@users.noreply.github.com>
2025-05-31 13:20:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
-        couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
+        buyer: '1057320665016971324', // Buyer
+        couleur: '#FF0000', // Couleur HEX
+        footer: 'Amor', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
