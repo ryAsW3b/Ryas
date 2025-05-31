ca83f904dc08aabb71da10867f3905b1611bc056
ryAsW3b <82412427+ryAsW3b@users.noreply.github.com>
2025-05-31 13:20:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const client = new Client({
     partials: ["USER", "CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION"]
 });
 
-client.login(client.process.token); // Tu remplacer par client.login("Le Token Du Bot")
+client.login(process.env.DISCORD_TOKEN);
 client.commands = new Collection();
 
 const { GiveawaysManager } = require('discord-giveaways');
